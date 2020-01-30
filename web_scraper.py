1a19eb487c202941dd6cd3b8c799d90cab1cf6c4
AdityaJsr <adityajsr1996@gmail.com>
2020-01-30 07:10:00 +0530

    fixed errors left type error list not str

--- web_scraper.py
+++ web_scraper.py
@@ -4,8 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ES
 from selenium.common.exceptions import TimeoutException
 
-option = webdriver.ChromeOptions
-option.add_argument('--incognito')
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
 
 browser = webdriver.Chrome(executable_path="C:\chrome driver\chromedriver_win32\chromedriver.exe", chrome_options= option)
 
@@ -14,12 +14,12 @@ browser.get('https://www.amazon.in/s?k=1660+super&crid=GJCRBFWHHE4R&sprefix=1660
 timeout = 10
 
 try:
-    WebDriverWait(browser, timeout).until(ES.visibility_of_element_located((By.XPATH,"//*[@id="search"]/div[1]/div[2]/div/span[4]/div[1]/div[10]/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span")))
+    WebDriverWait(browser, timeout).until(ES.visibility_of_element_located((By.XPATH,('//*[@id="p_72/1318476031"]/span/a/section/i'))))
 except TimeoutException:
     print("Oops Error Timeout")
     browser.quit()
 
-title_element = browser.find_elements_by_xpath('')
+title_element = browser.find_elements_by_xpath('//*[@id="search"]/div[1]/div/div/span/div/div/div/span/div/div/div/div/div/div/div/div/div/div/div/h2')
 title = [x.text for x in title_element]
 
 '''it can also be written as
@@ -29,14 +29,9 @@ for x in title_elements:
 
 print('Title'+title)
 
-value_element = browser.find_elements_by_xpath('')
+value_element = browser.find_elements_by_xpath('//*[@id="search"]/div/div/div/span/div/div/div/span/div/div/div/div/div/div/div/div/div/div/div/div/div/a/span')
 value = [x.text for x in value_element]
 print('Value'+value)
 
 for title,value in zip(title, value):
     print(title +' :' + value)
-
-
-
-
-
